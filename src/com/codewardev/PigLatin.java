e525af37607a0aa3bb7d8ce6ed3b783b0c06fb96
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-22 22:06:11 +1100

    first unsolved cut

---
+++ src/com/codewardev/PigLatin.java
@@ -0,0 +1,10 @@
+package com.codewardev;
+
+public class PigLatin {
+
+	public static String pigIt(String str) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ tests/com/codewardev/test/PigLatinTests.java
@@ -0,0 +1,17 @@
+package com.codewardev.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.codewardev.PigLatin;
+
+class PigLatinTests {
+
+	@Test
+    public void FixedTests() {
+        assertEquals("igPay atinlay siay oolcay", PigLatin.pigIt("Pig latin is cool"));
+        assertEquals("hisTay siay ymay tringsay", PigLatin.pigIt("This is my string"));
+    }
+
+}
