a2b821caf18d7c8dfb45a404c253b93bccbb7170
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-23 20:32:32 +1100

    refined version

--- src/com/codewardev/PigLatin.java
+++ src/com/codewardev/PigLatin.java
@@ -1,23 +1,17 @@
 package com.codewardev;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 // https://www.codewars.com/kata/simple-pig-latin/train/java
 
 public class PigLatin {
-
 	public static String pigIt(String str) {
-		String[] strings = str.split(" ");
-		String result ="";
-		for(String x: strings) {
-			if(x.matches("[a-zA-Z]+")) {
-				char first = x.charAt(0);
-				String newStr = x.replaceFirst(first+"", "").concat(first+"ay");
-				result += newStr+" ";
-				System.out.println(newStr);
-			} else {
-				result += x;
-			}
-		}
-		return result.trim();
+		return Arrays.stream(str.split(" "))
+				.map(x->{
+					char first = x.charAt(0);
+					return (x.matches("\\w+")? x.subSequence(1, x.length()).toString().concat(first+"ay"):x);
+				}).collect(Collectors.joining(" ")).toString().trim();
 	}
 
 }
